b8a24c7eac63a2c5fc91fc8f6547b221e4814133
Emanuel Zapata <emanuelzapata@ymail.com>
2023-07-05 09:11:17 -0500

    committing to push Java project

---
+++ src/main/java/com/emanuelzapata/quoter/QuoterApplication.java
@@ -0,0 +1,13 @@
+package com.emanuelzapata.quoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoterApplication.class, args);
+	}
+
+}
